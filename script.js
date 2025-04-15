94c4445b223071ae4749b9e858f5db11172c4d6c
shreyas162424 <savitasangalad@gmail.com>
2025-04-16 02:16:51 +0530

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example for smooth scroll if necessary
+document.querySelectorAll('.menu a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
